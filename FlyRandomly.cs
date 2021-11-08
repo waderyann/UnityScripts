4bd7e6073fb7f04631c2850e9402a13c4424831f
Ryan Martin <91626813+waderyann@users.noreply.github.com>
2021-11-08 17:34:42 -0500

    Add files via upload

---
+++ FlyRandomly.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyRandomly : MonoBehaviour
+{
+    float timeForNewPath = 1;
+    
+    public float amountOfAgents;
+    public float speed = 10.0f;
+    bool inCoroutine = false;
+    public GameObject Player;
+    public Transform Target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    float getRandomValue()
+    {
+        return Random.Range(-5, 5);
+    }
+
+
+    IEnumerator DoSomething()
+    {
+        inCoroutine = true;
+        yield return new WaitForSeconds(timeForNewPath);
+        GetNewPath();
+        inCoroutine = false;
+        //GetNewPath();
+    }
+
+    void GetNewPath()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(getRandomValue(), getRandomValue(), getRandomValue());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inCoroutine)
+        {
+            StartCoroutine(DoSomething());
+        }
+
+        //GetNewPath();
+
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z);
+            //this.transform.LookAt(Target.transform.position);
+        }
+
+        transform.LookAt(Target.transform.position);
+        transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+    }
+}
---
+++ MoveRandomly.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveRandomly : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float timeForNewPath;
+    public int newtarget;
+    public float speed;
+    public Vector3 Target;
+    bool inCoroutine = false;
+    NavMeshAgent navMeshAgent;
+
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    Vector3 getNewRandomPosition()
+    {
+        float x = Random.Range(-20, 20);
+        float y = Random.Range(-20, 20);
+        float z = Random.Range(-20, 20);
+
+        Vector3 pos = new Vector3(x, 3, z);
+        return pos;
+    }
+
+    IEnumerator DoSomething()
+    {
+        inCoroutine = true;
+        yield return new WaitForSeconds(timeForNewPath);
+        GetNewPath();
+        inCoroutine = false;
+        //GetNewPath();
+    }
+
+    void GetNewPath()
+    {
+        navMeshAgent.SetDestination(getNewRandomPosition());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inCoroutine)
+        {
+            StartCoroutine(DoSomething());
+        }
+    }
+}
