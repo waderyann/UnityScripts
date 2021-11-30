e15be3f984ab953b5ea03c694647f2f8b4f0879f
Ryan Martin <91626813+waderyann@users.noreply.github.com>
2021-11-30 17:36:54 -0500

    Webcam Script for adding onto Object Textures

---
+++ webcam.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class webcam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static WebCamTexture backCam;
+    //AudioSource audio = GetComponent<AudioSource>();
+   
+
+    void Start()
+    {
+        //audio = GetComponent<AudioSource>();
+        //audio.clip = Microphone.Start(null, true, 1, 22050);
+        //audio.loop = true;
+        //while (!(Microphone.GetPosition(null) > 0))
+        //{
+
+        //}
+        //audio.Play();
+
+        if (backCam == null)
+            backCam = new WebCamTexture();
+
+        GetComponent<Renderer>().material.mainTexture = backCam;
+
+        if (!backCam.isPlaying)
+            backCam.Play();
+
+    }
+
+    void Update()
+    {
+
+    }
+}
